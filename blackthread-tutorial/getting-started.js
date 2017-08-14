fefbd6c88732d770855ef2f1a292660a05a512a6
garrottkai <garrottkai@gmail.com>
2017-08-14 09:11:05 -0600

    change lighting, etc

--- blackthread-tutorial/getting-started.js
+++ blackthread-tutorial/getting-started.js
@@ -15,6 +15,27 @@ camera.position.set( 0, 0, 20 );
 
 var geometry = new THREE.TorusKnotBufferGeometry( 5, 1, 100, 16 );
 
-var material = new THREE.MeshBasicMaterial( {
+var material = new THREE.MeshStandardMaterial( {
     color: 0xb300b3,
 } );
+
+var mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+var ambientLight = new THREE.AmbientLight(0x666666, 1.0);
+scene.add(ambientLight);
+
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+directionalLight.position.set(0, 10, 0);
+scene.add(directionalLight);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
+
+    renderer.render(scene, camera);
+}
+animate();
